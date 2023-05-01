cc6a58a67be4454f6c3d46603132b1683e51a580
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2023-05-01 16:05:48 +0200

    Adding other Terraform steps

---
+++ cicd_bootstrap/conf/github_actions.tfvars
@@ -0,0 +1,2 @@
+github_org = "bdg-tbd"
+github_repo = "tbd-workshop-1"
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+  type = string
+  description = "Project name"
+}
+
+variable "region" {
+  type        = string
+  default = "europe-west1"
+  description = "GCP region"
+}
