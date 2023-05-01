ab9e826d14b61ba583837aacc3e80cf604f1747f
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2023-05-01 23:56:33 +0200

    feat: Add main VPC

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-
 resource "google_project_service" "notebooks" {
   provider           = google
   service            = "notebooks.googleapis.com"
   disable_on_destroy = true
+}
+
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "~> 7.0"
+  project_id   = var.project_name
+  network_name = "main-vpc"
+  routing_mode = "GLOBAL"
 }
