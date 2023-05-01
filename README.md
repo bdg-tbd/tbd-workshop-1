a456c59aaef6df1adc5c759ab8fa5cbd456bc9cf
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2023-05-02 00:02:40 +0200

    fix: Add egress

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ terraform apply -var-file ../env/project.tfvars -var-file conf/github_actions.tf
 
 | Name | Version |
 |------|---------|
-| <a name="provider_google"></a> [google](#provider\_google) | ~> 4.63.0 |
+| <a name="provider_google"></a> [google](#provider\_google) | 4.63.1 |
 
 ## Modules
 
--- main.tf
+++ main.tf
@@ -10,4 +10,20 @@ module "vpc" {
   project_id   = var.project_name
   network_name = "main-vpc"
   routing_mode = "GLOBAL"
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = var.region
+    },
+  ]
+  routes = [
+    {
+      name              = "egress-internet"
+      description       = "route through IGW to access internet"
+      destination_range = "0.0.0.0/0"
+      tags              = "egress-inet"
+      next_hop_internet = "true"
+    }
+  ]
 }
