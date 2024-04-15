16fc51d81e6dc769db94f3564f94bb40b7706b58
Daniel <d.braula@omtech.pl>
2024-04-15 23:00:08 +0200

    phase1 task 14.1

--- variables.tf
+++ variables.tf
@@ -13,3 +13,18 @@ variable "ai_notebook_instance_owner" {
   type        = string
   description = "Vertex AI workbench owner"
 }
+
+variable "dataproc_num_workers" {
+  type    = number
+  default = 2
+}
+
+variable "dataproc_machine_type" {
+  type    = string
+  default = "e2-standard-2"
+}
+
+variable "notebook_machine_type" {
+  type    = string
+  default = "e2-standard-2"
+}
