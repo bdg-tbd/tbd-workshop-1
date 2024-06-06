e1bfdbdf4b2e4d0cd269905afc5632ffc2b42d84
pinjesz_komputer <jasiekjeschke@gmail.com>
2024-06-06 18:55:31 +0200

    update branch name

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ locals {
   dbt_version             = "1.7.13"
   dbt_spark_version       = "1.7.1"
   dbt_git_repo            = "https://github.com/Pinjesz/tbd-tpc-di.git"
-  dbt_git_repo_branch     = "notebook"
+  dbt_git_repo_branch     = "main"
 }
 
 module "vpc" {
