2ddbc18715d28a0e0ed800ff1b1c4a0715f3e18f
kraszor <kraszewski111@gmail.com>
2024-11-12 23:19:28 +0100

    task 14.1

--- modules/dataproc/variables.tf
+++ modules/dataproc/variables.tf
@@ -20,6 +20,12 @@ variable "machine_type" {
   description = "Machine type to use for both worker and master nodes"
 }
 
+variable "worker_nodes" {
+  type        = number
+  default     = 2
+  description = "Number of nodes to use for worker nodes"
+}
+
 variable "image_version" {
   type    = string
   default = "2.1.27-ubuntu20"
