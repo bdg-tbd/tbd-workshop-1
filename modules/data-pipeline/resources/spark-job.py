33522b37ad8b3d93eade8145ca9d8b13ad58be40
Hubert Pogorzelski <hpogorzelski0@gmail.com>
2025-04-12 15:30:27 +0200

    Fix formatting after pre-commit hook and add Dataproc preemptible support

--- modules/data-pipeline/resources/spark-job.py
+++ modules/data-pipeline/resources/spark-job.py
@@ -18,8 +18,8 @@ import tempfile
 from pyspark.sql import SparkSession
 
 # change to your data bucket
-DATA_BUCKET = "gs://tbd-2025l-9900-data/data/shakespeare/"
 
+DATA_BUCKET = "gs://tbd-2025l-9923-data/scripts/"
 spark = SparkSession.builder.appName('Shakespeare WordCount').getOrCreate()
 
 table = 'bigquery-public-data.samples.shakespeare'
