e02a5a8c898c9641879cd0c212d91daf71b60ea0
Helena Celejowska <120586825+helacel@users.noreply.github.com>
2025-12-03 21:56:52 +0100

    Add secondary worker configuration to main.tf

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -134,5 +134,10 @@ resource "google_dataproc_cluster" "tbd-dataproc-cluster" {
       }
 
     }
+
+    secondary_worker_config {
+      num_instances  = 4          # możesz zmienić liczbę na inną, np. 2
+      machine_type   = var.machine_type
+      preemptibility = "PREEMPTIBLE"
   }
 }
